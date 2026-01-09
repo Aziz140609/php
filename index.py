bf24f03f0e0468a7ddc8289ee63688c0c30357ba
Rizafii <firmansyah.riza6@gmail.com>
2026-01-09 13:55:05 +0700

    first commit

---
+++ index.py
@@ -0,0 +1,25 @@
+#print biasa
+print("Hello, World!")
+
+#print boolean
+Benar = True
+Salah = False
+print(Benar)
+
+#menampilkan tipe data
+x = 10
+print(type(x)) 
+
+#list dan mengubah nilai list
+buah =["apel", "pisang", "jeruk"]
+buah[0] = "markisa"
+print(buah)
+
+#list yg tidak bisa di ubah
+angka = (1, 2, 3, 4, 5,5)
+#angka(0) = 10  # ini akan error karena tuple tidak bisa di ubah
+print(angka)
+
+#set yg tidak bisa duplikat nilai
+huruf = {'a', 'b', 'c', 'c'}
+print(huruf)
